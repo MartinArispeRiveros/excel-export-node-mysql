6ffa81d2020c45df64e38e07afab667060efe1c4
Samuel_UCB <val97samuel@gmail.com>
2018-02-20 05:53:21 -0300

    Created table CardAccess in bookshelf

--- db/bookshelf.js
+++ db/bookshelf.js
@@ -4,6 +4,9 @@ var Artist = bookshelf.Model.extend({
   tableName: 'Artist'
 });
 
+var CardAccess = bookshelf.Model.extend({
+  tableName: 'card_access'
+})
 
 // example
 Artist.where('artist_id', 1).fetch().then(function(user) {
