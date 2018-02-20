1df78632e8f0427dae7bbeb334fe54ad49c75397
Samuel_UCB <val97samuel@gmail.com>
2018-02-20 05:47:23 -0300

    Created table Artist in bookshelf

--- db/bookshelf.js
+++ db/bookshelf.js
@@ -1,2 +1,11 @@
 var bookshelf = require('./knex');
 
+var Artist = bookshelf.Model.extend({
+  tableName: 'Artist'
+});
+
+
+// example
+Artist.where('artist_id', 1).fetch().then(function(user) {
+  console.log(user.toJSON());
+});
