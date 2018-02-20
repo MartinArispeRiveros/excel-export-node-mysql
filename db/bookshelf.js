5f296068ae629fad52db923eecb58ae4646e0398
Samuel_UCB <val97samuel@gmail.com>
2018-02-20 06:57:10 -0300

    Created table CardAccessType and Event in bookshelf

--- db/bookshelf.js
+++ db/bookshelf.js
@@ -1,14 +1,33 @@
 var bookshelf = require('./knex');
 
 var Artist = bookshelf.Model.extend({
-  tableName: 'Artist'
+  tableName   : 'Artist',
+  tableName   : 'artist_id'
 });
 
 var CardAccess = bookshelf.Model.extend({
-  tableName: 'card_access'
-})
+  tableName   : 'card_access',
+  idAttribute : 'card_access_id'
+});
+
+var CardAccessType = bookshelf.Model.extend({
+  tableName   : 'card_access_type',
+  idAttribute : 'card_access_type_id',
+  event      : function() {
+    return this.belongsTo(Event, 'event_id');
+  }
+});
+
+var Event = bookshelf.Model.extend({
+  tableName       : 'event',
+  idAttribute     : 'event_id',
+  cardAccessTypes : function() {
+    return this.hasMany(CardAccessType, 'card_access_id');
+  }
+});
+  
 
 // example
-Artist.where('artist_id', 1).fetch().then(function(user) {
-  console.log(user.toJSON());
+CardAccessType.where({card_access_type_id: 2}).fetch({withRelated: ['event']}).then(function(cardAccessType) {
+  console.log(cardAccessType.related('event').toJSON());
 });
