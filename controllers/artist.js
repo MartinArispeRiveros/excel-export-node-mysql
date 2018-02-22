040276b697abbf9e5b668f587aa90f00dd6d8c62
SamuelUCB <val97samuel@hotmail.com>
2018-02-22 16:41:04 -0300

    Created getAllArtistsFunction

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router  = express.Router();
 
-var Artist  = require('../models/Artist');
-var Excel   = require('../helpers/Excel');
+//var ArtistRepository = require('../repositories/ArtistRepository');
+var Artist           = require('../models/Artist');
+var Excel            = require('../helpers/Excel');
 
 router.get('/', function(req, res) {
+  //var collection = ArtistRepository.getAllArtists();
   Artist.collection().fetch().then(function(collection) {
     res.setHeader('Content-disposition', 'attachment; filename=Artist.xlsx');
     res.send(Excel.getBuffer(collection.toJSON()));
-    console.log('Table Artist to excel');
   });
+  console.log('Table Artist to excel');
 });
 
 module.exports = router;
--- repositories/ArtistRepository.js
+++ repositories/ArtistRepository.js
@@ -1,9 +1,13 @@
+var Artist = require('../models/Artist');
+
 class ArtistRepository {
 
-    static getAllArtists() {
+  static getAllArtists() {
+    return Artist.collection().fetch().then(function(collection) {
+      return collection.toJSON();
+    });
+  }
 
-    }
-    
 }
 
 module.exports = ArtistRepository;
