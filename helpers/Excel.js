dc1b9026a2ed8c12d5b895521cc39c50d7aeecd1
SamuelUCB <val97samuel@hotmail.com>
2018-02-22 14:49:24 -0300

    Added default value for name in getBuffer

--- helpers/Excel.js
+++ helpers/Excel.js
@@ -2,18 +2,18 @@ var xlsx = require('node-xlsx').default;
 
 class Excel {
 
-  static getBuffer(collection) {
+  static getBuffer (collection, name = 'main') {
     var data = [];
     for (var index = 0; index < collection.length; index++) {
       var dataRow = [];
-      for(var value in collection[index]) {
+      for (var value in collection[index]) {
         dataRow.push(collection[index][value]);
       }
       data.push(dataRow);
     }
-    return xlsx.build([{name: "main", data: data}]);
+    return xlsx.build([{name: name, data: data}]);
   }
-  
+
 }
 
 module.exports = Excel;
