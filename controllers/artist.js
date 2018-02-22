826d3c35fb4317e77be2d54859fe75960c432fae
SamuelUCB <val97samuel@hotmail.com>
2018-02-22 14:13:50 -0300

    Refactor artist controller

--- controllers/artist.js
+++ controllers/artist.js
@@ -1,24 +1,13 @@
 var express = require('express');
 var router  = express.Router();
 
-var xlsx    = require('node-xlsx').default;
 var Artist  = require('../models/artist');
-var excel   = require('../helpers/Excel');
+var Excel   = require('../helpers/Excel');
 
 router.get('/', function(req, res) {
   Artist.collection().fetch().then(function(collection) {
-    var rows = collection.toJSON();
-    var data = [];
-    for (var index = 0; index < rows.length; index++) {
-      var dataRow = [];
-      for(var value in rows[index]) {
-        dataRow.push(rows[index][value]);
-      }
-      data.push(dataRow);
-    }
-    var buffer = xlsx.build([{name: "main", data: data}]);
     res.setHeader('Content-disposition', 'attachment; filename=Artist.xlsx');
-    res.send(buffer);
+    res.send(Excel.getBuffer(collection.toJSON()));
     console.log('Table Artist to excel');
   });
 });
--- helpers/Excel.js
+++ helpers/Excel.js
@@ -1,5 +1,19 @@
+var xlsx = require('node-xlsx').default;
+
 class Excel {
-    
+
+  static getBuffer(collection) {
+    var data = [];
+    for (var index = 0; index < collection.length; index++) {
+      var dataRow = [];
+      for(var value in collection[index]) {
+        dataRow.push(collection[index][value]);
+      }
+      data.push(dataRow);
+    }
+    return xlsx.build([{name: "main", data: data}]);
+  }
+  
 }
 
 module.exports = Excel;
